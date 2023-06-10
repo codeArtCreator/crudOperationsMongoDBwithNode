0f68c11911c4263c3e550296f452317cb1a6a29c
codeartcreator <rakeshpr369@gmail.com>
2023-06-10 15:57:01 +0530

    GET and POST

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 
 const app = express()
 const mobiles = JSON.parse(fs.readFileSync('./data/mobiles.json'))
+app.use(express.json())
 
 //GET - api/mobiles
 app.get('/api/v1/mobiles', (req, res) => {
@@ -11,6 +12,23 @@ app.get('/api/v1/mobiles', (req, res) => {
         data: {
             mobiles: mobiles
         }
+    })
+})
+
+//POST - api/mobiles
+app.post('/api/v1/mobiles', (req, res) => {
+    const newId = mobiles[mobiles.length - 1].id + 1
+    const newMobile = Object.assign({ id: newId }, req.body)
+    console.log(req.body);
+
+    mobiles.push(newMobile)
+    fs.writeFile('./data/mobiles.json', JSON.stringify(mobiles), (err) => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                mobile: newMobile,
+            }
+        })
     })
 })
 
